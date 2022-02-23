f6fc4337dbcebe335868ff3453dcb8cb9513c4e8
JorgeMasson <j_masson_reynoso@hotmail.com>
2022-02-23 16:03:58 -0700

    Data class pelicula

---
+++ app/src/main/java/masson/reynoso/popcornfactory/Pelicula.kt
@@ -0,0 +1,6 @@
+package masson.reynoso.popcornfactory
+
+data class Pelicula (var titulo: String,
+                     var imagen: Int,
+                     var header: Int,
+                     var sinopsis: String)
