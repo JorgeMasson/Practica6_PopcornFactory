bd6f419e27715ed77340d5f7318d281c6c47c7b9
JorgeMasson <j_masson_reynoso@hotmail.com>
2022-02-23 16:28:45 -0700

    Main activity terminada

--- app/src/main/java/masson/reynoso/popcornfactory/MainActivity.kt
+++ app/src/main/java/masson/reynoso/popcornfactory/MainActivity.kt
@@ -1,11 +1,75 @@
 package masson.reynoso.popcornfactory
 
+import android.content.Context
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.BaseAdapter
+import kotlinx.android.synthetic.main.activity_detalle_pelicula.view.*
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.pelicula.view.*
 
 class MainActivity : AppCompatActivity() {
+    var adapter: PeliculaAdapter? = null
+    var peliculas = ArrayList<Pelicula>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        cargarPeliculas()
+
+        adapter = PeliculaAdapter(this, peliculas)
+        grid_view.adapter = adapter
     }
+
+    fun cargarPeliculas() {
+
+    }
+}
+
+class PeliculaAdapter: BaseAdapter {
+    var peliculas = ArrayList<Pelicula>()
+    var context: Context? = null
+
+    constructor(context: Context, peliculas: ArrayList<Pelicula>) {
+        this.context = context
+        this.peliculas = peliculas
+    }
+
+    override fun getCount(): Int {
+        return peliculas.size
+    }
+
+    override fun getItem(p0: Int): Any {
+        return peliculas[p0]
+    }
+
+    override fun getItemId(p0: Int): Long {
+        return p0.toLong()
+    }
+
+    override fun getView(p0: Int, p1: View?, p2: ViewGroup?): View {
+        var pelicula = peliculas[p0]
+        var inflator = context!!.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
+        var vista = inflator.inflate(R.layout.pelicula, null)
+        vista.iv_pelicula.setImageResource(pelicula.imagen)
+        vista.tv_titulo_pelicula.setText(pelicula.titulo)
+
+        vista.iv_pelicula.setOnClickListener {
+            var intent = Intent(context, DetallePelicula::class.java)
+
+            intent.putExtra("titulo", pelicula.titulo)
+            intent.putExtra("imagen", pelicula.imagen)
+            intent.putExtra("header", pelicula.header)
+            intent.putExtra("sinopsis", pelicula.sinopsis)
+            context!!.startActivity(intent)
+        }
+
+        return vista
+    }
+
 }
